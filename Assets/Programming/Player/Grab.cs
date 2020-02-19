cd7e654c861a26a11b7dc2ccbc24e9ccf1b17886
Kyn21kx <nefesmeralda@gmail.com>
2020-02-18 20:49:13 -0600

    Update to programming

--- Assets/Programming/Player/Grab.cs
+++ Assets/Programming/Player/Grab.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class Grab : MonoBehaviour
 {
     #region Variables
-
+    Camera cam;
+    public float grabbingDistance;
     #endregion
 
     private void Update() {
-        
+        cam = Camera.main;
+        GrabObject();
     }
 
     private void GrabObject () {
-        
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, grabbingDistance)) {
+            if (hit.transform != null) {
+                Debug.Log(hit.transform.name);
+            }
+        }
+        Debug.DrawRay(cam.transform.position, cam.transform.forward * grabbingDistance);
     }
 }
