d3c7fd850d3fb6358ddbd71d76332feb8dc19c7f
Kyn21kx <nefesmeralda@gmail.com>
2020-02-20 11:25:24 -0600

    Rotation finished

--- Assets/Programming/Auxiliar/ObjectPlacing.cs
+++ Assets/Programming/Auxiliar/ObjectPlacing.cs
@@ -6,11 +6,49 @@ public class ObjectPlacing : MonoBehaviour
 {
     [SerializeField]
     private float radius;
-    public float extraHitBox;
+    public bool inRadius;
+    [SerializeField]
+    private float returnSpeed;
+    [SerializeField]
+    private float proximitySnap;
+    [Header("Object that will be dragged to this")]
+    [SerializeField]
+    private Rigidbody objRig;
+    private Grab grabRef;
+    private float dis;
+
+    private void Start() {
+        grabRef = GameObject.FindGameObjectWithTag("Player").GetComponent<Grab>();
+    }
 
     private void Update() {
-        RaycastHit hit;
-           
+        if (!grabRef.grabbed) {
+            RadiusBehaviour();
+        }
+    }
+
+    private void RadiusBehaviour () {
+        dis = Vector3.Distance(transform.position, objRig.transform.position);
+        if (dis <= radius) {
+            inRadius = true;
+        }
+        if (dis > radius) {
+            inRadius = false;
+            objRig.isKinematic = false;
+        }
+        if (inRadius) {
+            objRig.isKinematic = true;
+            objRig.transform.rotation = Quaternion.Lerp(objRig.transform.rotation, Quaternion.Euler(new Vector3(-90f, 0f, 0f)), Time.deltaTime * returnSpeed);
+            objRig.transform.position = Vector3.Slerp(objRig.transform.position, transform.position, Time.deltaTime * returnSpeed);
+            if (dis < radius / proximitySnap) {
+                objRig.transform.position = transform.position;
+            }
+        }
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, radius);
     }
 
 }
