452b2ccb334e0bf157b69446da092f11fb5e727f
Kyn21kx <nefesmeralda@gmail.com>
2020-02-19 21:55:41 -0600

    yrh

---
+++ Assets/Programming/Auxiliar/ObjectPlacing.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPlacing : MonoBehaviour
+{
+    [SerializeField]
+    private float radius;
+    public float extraHitBox;
+
+    private void Update() {
+        RaycastHit hit;
+           
+    }
+
+}
