49e4372f224b4bc1ab357b7ee60d02686619cc33
Kyn21kx <nefesmeralda@gmail.com>
2020-02-22 09:31:19 -0600

    Order manager update
    You can now order different products for the customers

--- Assets/Programming/Player/OrderManager.cs
+++ Assets/Programming/Player/OrderManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using TMPro;
 using System.Collections.Generic;
 
 public class OrderManager : MonoBehaviour
@@ -9,9 +10,9 @@ public class OrderManager : MonoBehaviour
     #region Variables
     public int numberOfIngredientes;
     [SerializeField]
-    Ingredients[] ingredients;
-    [SerializeField]
     List<int> repeatedValues = new List<int>();
+    public List<Ingredients[]> orders = new List<Ingredients[]>();
+    public TextMeshProUGUI orderText;
     #endregion
 
     private void Update() {
@@ -21,11 +22,24 @@ public class OrderManager : MonoBehaviour
     }
 
     private void GenerateOrder () {
+        orders.Add(GenerateGordita());
+        orderText.SetText("");
+        for (int j = 0; j < orders.Count; j++) {
+            var item = orders[j];
+            orderText.text +="\nOrden #" + (j + 1) + ". Deme una gordita con: ";
+            for (int i = 0; i < item.Length; i++) {
+                orderText.text += (item.GetValue(i) + " ");
+            }
+        }
+    }
+
+    private Ingredients[] GenerateGordita () {
         //Return the array of the ingredients selected
         System.Random ingNum = new System.Random();
         System.Random ing = new System.Random();
         numberOfIngredientes = ingNum.Next(1, System.Enum.GetNames(typeof(Ingredients)).Length);
         var values = System.Enum.GetValues(typeof(Ingredients));
+        Ingredients[] ingredients;
         ingredients = new Ingredients[numberOfIngredientes];
         repeatedValues.Clear();
         for (int i = 0; i < ingredients.Length; i++) {
@@ -34,12 +48,12 @@ public class OrderManager : MonoBehaviour
                 var newIng = (Ingredients)values.GetValue(init);
                 ingredients.SetValue(newIng, i);
                 repeatedValues.Add(init);
-                Debug.Log(init);
             }
             else {
                 i--;
             }
         }
+        return ingredients;
     }
 
 }
