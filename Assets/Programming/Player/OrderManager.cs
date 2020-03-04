fa150204d413b1724512a413595853bd4a445257
Kyn21kx <nefesmeralda@gmail.com>
2020-03-03 22:06:51 -0600

    uuuuh

--- Assets/Programming/Player/OrderManager.cs
+++ Assets/Programming/Player/OrderManager.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using TMPro;
 using System.Collections.Generic;
+using System.Collections;
 
 public class OrderManager : MonoBehaviour {
     //Set up orders by a minimum number of ingredients, core ingredients, and types of food
@@ -12,39 +13,41 @@ public class OrderManager : MonoBehaviour {
     public int maxNumberOfProducts;
     [SerializeField]
     List<int> repeatedValues = new List<int>();
-    public List<Ingredients[]> products = new List<Ingredients[]>();
     public List<List<Ingredients[]>> orders = new List<List<Ingredients[]>>();
     public TextMeshProUGUI orderText;
     private System.Random ing;
+    [SerializeField]
+    private int orderIndex = 0;
     #endregion
-
+    /*
+     * TO DO SUPER IMPORTAAAAAAAAAAANT
+     * Replace the use of global variables for the return of every method, because the functions are returning literally all of the values on the arrays
+     */
     private void Update() {
+        //Replace input event for when a customer walks in to ask for an order
         if (Input.GetKeyDown(KeyCode.E)) {
-            numberOfProducts = new System.Random().Next(1, maxNumberOfProducts);
-            for (int i = 0; i < numberOfProducts; i++) {
-                GenerateOrder();
-            }
+            StartCoroutine(GenerateOrder());
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            ViewOrders();
         }
     }
 
-    private void GenerateOrder() {
-        orders.Add(GenerateProduct());
-    }
-
-    private List<Ingredients[]> GenerateProduct () {
-        products.Add(GenerateGordita());
-        orderText.SetText("");
-        for (int i = 0; i < products.Count; i++) {
-            orderText.text += "\nProducto: ";
-            var item = products[i];
-            for (int j = 0; j < item.Length; j++) {
-                orderText.text += item.GetValue(j) + " ";
-            }
+    private IEnumerator GenerateOrder() {
+        numberOfProducts = new System.Random().Next(1, maxNumberOfProducts);
+        List<Ingredients[]> products = new List<Ingredients[]>();
+        for (int i = 0; i < numberOfProducts; i++) {
+            orders.Add(GenerateProduct(products));
+            yield return new WaitForFixedUpdate();
         }
-        return products;
     }
 
-    private Ingredients[] GenerateGordita () {
+    private List<Ingredients[]> GenerateProduct(List<Ingredients[]> p) {
+        p.Add(GenerateGordita());
+        return p;
+    }
+
+    private Ingredients[] GenerateGordita() {
         //Return the array of the ingredients selected
         System.Random ingNum = new System.Random();
         ing = new System.Random();
@@ -64,6 +67,20 @@ public class OrderManager : MonoBehaviour {
             }
         }
         return ingredients;
+    }
+
+    private void ViewOrders() {
+        orderText.SetText("");
+        orderText.text += "\nOrder #" + (orderIndex + 1) + ": "; 
+        for (int j = 0; j < orders[orderIndex].Count; j++) {
+            //Iterating products
+            orderText.text += "Product #" + (j + 1) + ": ";
+            for (int k = 0; k < orders[orderIndex][j].Length; k++) {
+                //Iterating ingredients
+                orderText.text += orders[orderIndex][j][k] + " ";
+            }
+        }
+        //products.Clear();
     }
 
 }
